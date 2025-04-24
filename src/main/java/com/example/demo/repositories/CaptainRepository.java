da6e60f8dc1f70310c7fffcdf87444bffe505812
mariareda <121574095+mariareda@users.noreply.github.com>
2025-04-24 18:28:37 +0200

    Add Captain repository

--- src/main/java/com/example/demo/repositories/CaptainRepository.java
+++ src/main/java/com/example/demo/repositories/CaptainRepository.java
@@ -1,4 +1,13 @@
 package com.example.demo.repositories;
 
-public interface CaptainRepository {
+import com.example.demo.models.Captain;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CaptainRepository extends JpaRepository<Captain, Long> {
+    List<Captain> findByAvgRatingScoreGreaterThan(Double ratingThreshold);
+    Captain findByLicenseNumber(String licenseNumber);
 }
