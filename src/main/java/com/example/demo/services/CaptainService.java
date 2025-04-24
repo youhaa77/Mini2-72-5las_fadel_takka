3fae805572dc6b15cc16e27ee1ce8da829dffec3
mariareda <121574095+mariareda@users.noreply.github.com>
2025-04-24 18:40:44 +0200

    Add Captain Services

--- src/main/java/com/example/demo/services/CaptainService.java
+++ src/main/java/com/example/demo/services/CaptainService.java
@@ -1,4 +1,35 @@
 package com.example.demo.services;
 
+import com.example.demo.repositories.CaptainRepository;
+import org.springframework.stereotype.Service;
+import com.example.demo.models.Captain;
+import java.util.List;
+
+@Service
 public class CaptainService {
+
+    private final CaptainRepository captainRepository;
+    public CaptainService(CaptainRepository captainRepository) {
+        this.captainRepository = captainRepository;
+    }
+
+    public Captain addCaptain(Captain captain) {
+        return captainRepository.save(captain);
+    }
+
+    public List<Captain> getAllCaptains() {
+        return captainRepository.findAll();
+    }
+
+    public Captain getCaptainById(Long id) {
+        return captainRepository.findById(id).orElse(null);
+    }
+
+    public List<Captain> getCaptainsByRating(Double ratingThreshold) {
+        return captainRepository.findByAvgRatingScoreGreaterThan(ratingThreshold);
+    }
+
+    public Captain getCaptainByLicenseNumber(String licenseNumber) {
+        return captainRepository.findByLicenseNumber(licenseNumber);
+    }
 }
