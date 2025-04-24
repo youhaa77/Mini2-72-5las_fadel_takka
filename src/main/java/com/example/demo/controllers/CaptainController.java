384a629c14784c4746b3b479a029314000a3ac5f
mariareda <121574095+mariareda@users.noreply.github.com>
2025-04-24 18:53:25 +0200

    Add Captain Controller

--- src/main/java/com/example/demo/controllers/CaptainController.java
+++ src/main/java/com/example/demo/controllers/CaptainController.java
@@ -1,4 +1,43 @@
 package com.example.demo.controllers;
+import com.example.demo.models.Captain;
+import com.example.demo.services.CaptainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequestMapping("/captain")
 public class CaptainController {
+
+    private final CaptainService captainService;
+
+    @Autowired
+    public CaptainController(CaptainService captainService) {
+        this.captainService = captainService;
+    }
+
+    @PostMapping("/addCaptain")
+    public Captain addCaptain(@RequestBody Captain captain) {
+        return captainService.addCaptain(captain);
+    }
+
+    @GetMapping("/allCaptains")
+    public List<Captain> getAllCaptains() {
+        return captainService.getAllCaptains();
+    }
+
+    @GetMapping("/{id}")
+    public Captain getCaptainById(@PathVariable Long id) {
+        return captainService.getCaptainById(id);
+    }
+
+    @GetMapping("/filterByRating")
+    public List<Captain> getCaptainsByRating(@RequestParam Double ratingThreshold) {
+        return captainService.getCaptainsByRating(ratingThreshold);
+    }
+
+    @GetMapping("/filterByLicenseNumber")
+    public Captain getCaptainByLicenseNumber(@RequestParam String licenseNumber) {
+        return captainService.getCaptainByLicenseNumber(licenseNumber);
+    }
 }
